97b3f3ba35c056f0c23f50eedd5235b3fdf86d62
Lakiza21 <Lakizaza1293@mail.ru>
2021-08-01 21:57:28 +0300

    тест кек

---
+++ less1.py
@@ -0,0 +1,10 @@
+#okay
+
+a = int(input("введите длительность эксперимента (сек): ")) 
+print(a)
+
+b = (int(a // 60))
+c = (a % 60) * 60
+d = (a // 3600)
+
+simple_str = f"Okay, your experiment lasted for {a} seconds. You've spend {d}:{b}:{c} doing this shit"
